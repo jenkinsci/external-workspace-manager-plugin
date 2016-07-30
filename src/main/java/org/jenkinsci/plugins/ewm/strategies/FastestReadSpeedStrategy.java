a87b839be69ba8f184bb3a2d70a58e43d32b7e31
Alexandru Somai <somai.alexandru@gmail.com>
2016-07-30 17:52:26 +0300

    [JENKINS-36899] Implement fastest write/read speed allocation strategies

---
+++ src/main/java/org/jenkinsci/plugins/ewm/strategies/FastestReadSpeedStrategy.java
@@ -0,0 +1,49 @@
+package org.jenkinsci.plugins.ewm.strategies;
+
+import hudson.Extension;
+import org.jenkinsci.plugins.ewm.DiskAllocationStrategy;
+import org.jenkinsci.plugins.ewm.DiskAllocationStrategyDescriptor;
+import org.jenkinsci.plugins.ewm.Messages;
+import org.jenkinsci.plugins.ewm.definitions.Disk;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Selects the {@link Disk} with the highest {@link org.jenkinsci.plugins.ewm.DiskInfoProvider#readSpeed}.
+ *
+ * @author Alexandru Somai
+ */
+public class FastestReadSpeedStrategy extends DiskAllocationStrategy {
+
+    @Nonnull
+    @Override
+    public Disk allocateDisk(@Nonnull List<Disk> disks) throws IOException {
+        return Collections.max(disks, new Comparator<Disk>() {
+
+            @Override
+            public int compare(Disk disk1, Disk disk2) {
+                Double disk1ReadSpeed = disk1.getDiskInfo().getReadSpeed();
+                Double disk2ReadSpeed = disk2.getDiskInfo().getReadSpeed();
+
+                if (disk1ReadSpeed == null || disk2ReadSpeed == null) {
+                    return 0;
+                }
+                return Double.compare(disk1ReadSpeed, disk2ReadSpeed);
+            }
+        });
+    }
+
+    @Extension
+    public static class DescriptorImpl extends DiskAllocationStrategyDescriptor {
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return Messages.strategies_FastestReadSpeed_DisplayName();
+        }
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/ewm/strategies/FastestWriteSpeedStrategy.java
@@ -0,0 +1,49 @@
+package org.jenkinsci.plugins.ewm.strategies;
+
+import hudson.Extension;
+import org.jenkinsci.plugins.ewm.DiskAllocationStrategy;
+import org.jenkinsci.plugins.ewm.DiskAllocationStrategyDescriptor;
+import org.jenkinsci.plugins.ewm.Messages;
+import org.jenkinsci.plugins.ewm.definitions.Disk;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Selects the {@link Disk} with the highest {@link org.jenkinsci.plugins.ewm.DiskInfoProvider#writeSpeed}.
+ *
+ * @author Alexandru Somai
+ */
+public class FastestWriteSpeedStrategy extends DiskAllocationStrategy {
+
+    @Nonnull
+    @Override
+    public Disk allocateDisk(@Nonnull List<Disk> disks) throws IOException {
+        return Collections.max(disks, new Comparator<Disk>() {
+
+            @Override
+            public int compare(Disk disk1, Disk disk2) {
+                Double disk1WriteSpeed = disk1.getDiskInfo().getWriteSpeed();
+                Double disk2WriteSpeed = disk2.getDiskInfo().getWriteSpeed();
+
+                if (disk1WriteSpeed == null || disk2WriteSpeed == null) {
+                    return 0;
+                }
+                return Double.compare(disk1WriteSpeed, disk2WriteSpeed);
+            }
+        });
+    }
+
+    @Extension
+    public static class DescriptorImpl extends DiskAllocationStrategyDescriptor {
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return Messages.strategies_FastestWriteSpeed_DisplayName();
+        }
+    }
+}
