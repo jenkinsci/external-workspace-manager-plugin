a86a4310ab3a45183c5b67fe86bf2d5d29dcc3fd
Alexandru Somai <somai.alexandru@gmail.com>
2019-10-29 20:23:32 +0200

    [JENKINS-59347] Move acceptance tests from ATH

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,1 +1,15 @@
 buildPlugin()
+
+stage("UI tests") {
+    node('docker && highmem') {
+        checkout scm
+        docker.image('jenkins/ath:acceptance-test-harness-1.65').inside('-v /var/run/docker.sock:/var/run/docker.sock --shm-size 2g') {
+            sh """
+                mvn clean package -DskipTests # Build .hpi before running ATH so the snapshot is consumed instead of latest released
+                eval \$(vnc.sh)
+                mvn test -B -Dmaven.test.failure.ignore=true -DforkCount=1 -Ptest-ath
+            """
+        }
+        junit '**/target/surefire-reports/**/*.xml'
+    }
+}
---
+++ src/test/java/org/jenkinsci/plugins/ewm/acceptance/ExternalWorkspaceManagerPluginTest.java
@@ -0,0 +1,170 @@
+package org.jenkinsci.plugins.ewm.acceptance;
+
+import org.jenkinsci.plugins.ewm.acceptance.config.ExternalGlobalConfig;
+import org.jenkinsci.plugins.ewm.acceptance.config.ExternalNodeConfig;
+import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.junit.WithPlugins;
+import org.jenkinsci.test.acceptance.plugins.maven.MavenInstallation;
+import org.jenkinsci.test.acceptance.po.Build;
+import org.jenkinsci.test.acceptance.po.Slave;
+import org.jenkinsci.test.acceptance.po.WorkflowJob;
+import org.jenkinsci.test.acceptance.slave.LocalSlaveController;
+import org.jenkinsci.test.acceptance.slave.SlaveController;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.apache.commons.io.FileUtils.listFiles;
+import static org.apache.commons.io.filefilter.FileFilterUtils.directoryFileFilter;
+import static org.apache.commons.io.filefilter.FileFilterUtils.nameFileFilter;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Acceptance tests for External Workspace Manager Plugin.
+ *
+ * @author Alexandru Somai
+ */
+@WithPlugins({"workflow-aggregator", "external-workspace-manager", "run-selector", "ws-cleanup", "git"})
+public class ExternalWorkspaceManagerPluginTest extends AbstractJUnitTest {
+
+    @ClassRule
+    public static TemporaryFolder tmp = new TemporaryFolder();
+
+    private static final String DISK_POOL_ID = "diskpool1";
+    private static final String DISK_ONE = "disk1";
+    private static final String DISK_TWO = "disk2";
+    private static final String MOUNT_FROM_MASTER_TO_DISK_ONE = "/fake-mount-point-1";
+    private static final String MOUNT_FROM_MASTER_TO_DISK_TWO = "/fake-mount-point-2";
+
+    private String fakeNodeMountingPoint;
+
+    @Before
+    public void setUp() throws Exception {
+        MavenInstallation.installMaven(jenkins, "M3", "3.1.0");
+
+        setUpGlobalConfig();
+
+        fakeNodeMountingPoint = tmp.newFolder().getAbsolutePath();
+        setUpNode("linux", fakeNodeMountingPoint);
+        setUpNode("test", fakeNodeMountingPoint);
+    }
+
+    @Test
+    public void shareWorkspaceOneJobTwoNodes() {
+        WorkflowJob job = createWorkflowJob(String.format(
+                "def extWorkspace = exwsAllocate '%s' \n" +
+                        "node ('linux') { \n" +
+                        "   exws (extWorkspace) { \n" +
+                        "      writeFile file: 'marker', text: 'content' \n" +
+                        "   } \n" +
+                        "} \n" +
+                        "node ('test') { \n" +
+                        "   exws (extWorkspace) { \n" +
+                        "       def content = readFile(file: 'marker') \n" +
+                        "       if (content != 'content') error('Content mismatch: ' + content) \n" +
+                        "   } \n" +
+                        "}", DISK_POOL_ID));
+
+        Build build = job.startBuild();
+        build.shouldSucceed();
+        assertThat(build.getConsole(), containsString(String.format("Running in %s/%s/%s", fakeNodeMountingPoint, job.name, build.getNumber())));
+
+        verifyExternalWorkspacesAction(job.name, build);
+    }
+
+    @Test
+    public void shareWorkspaceTwoJobsTwoNodes() {
+        WorkflowJob upstreamJob = createWorkflowJob(String.format(
+                "def extWorkspace = exwsAllocate '%s' \n" +
+                        "node ('linux') { \n" +
+                        "   exws (extWorkspace) { \n" +
+                        "      writeFile file: 'marker', text: 'content' \n " +
+                        "   } \n" +
+                        "}", DISK_POOL_ID));
+
+        Build upstreamBuild = upstreamJob.startBuild();
+        upstreamBuild.shouldSucceed();
+        assertThat(upstreamBuild.getConsole(), containsString(String.format("Running in %s/%s/%s", fakeNodeMountingPoint, upstreamJob.name, upstreamBuild.getNumber())));
+        verifyExternalWorkspacesAction(upstreamJob.name, upstreamBuild);
+
+        WorkflowJob downstreamJob = createWorkflowJob(String.format("" +
+                "def run = selectRun '%s' \n" +
+                "def extWorkspace = exwsAllocate selectedRun: run \n" +
+                "node ('test') { \n" +
+                "   exws (extWorkspace) { \n" +
+                "       def content = readFile(file: 'marker') \n" +
+                "       if (content != 'content') error('Content mismatch: ' + content) \n" +
+                "   } \n" +
+                "}", upstreamJob.name));
+
+        Build downstreamBuild = downstreamJob.startBuild();
+        downstreamBuild.shouldSucceed();
+        assertThat(downstreamBuild.getConsole(), containsString(String.format("Running in %s/%s/%s", fakeNodeMountingPoint, upstreamJob.name, upstreamBuild.getNumber())));
+        verifyExternalWorkspacesAction(upstreamJob.name, downstreamBuild);
+    }
+
+    @Test
+    public void externalWorkspaceCleanup() {
+        WorkflowJob job = createWorkflowJob(String.format("" +
+                "def extWorkspace = exwsAllocate '%s' \n" +
+                "node ('linux') { \n" +
+                "	exws (extWorkspace) { \n" +
+                "		try { \n" +
+                "           writeFile file: 'foobar.txt', text: 'any' \n" +
+                "		} finally { \n" +
+                "			step ([$class: 'WsCleanup']) \n" +
+                "		} \n" +
+                "	} \n" +
+                "}", DISK_POOL_ID));
+
+        Build build = job.startBuild();
+        build.shouldSucceed();
+        String console = build.getConsole();
+        assertThat(console, containsString(String.format("Running in %s/%s/%s", fakeNodeMountingPoint, job.name, build.getNumber())));
+        assertThat(console, containsString("[WS-CLEANUP] Deleting project workspace"));
+        assertThat(console, containsString("[WS-CLEANUP] done"));
+        assertThat(listFiles(tmp.getRoot(), nameFileFilter("foobar.txt"), directoryFileFilter()), hasSize(0));
+    }
+
+    private void setUpGlobalConfig() {
+        jenkins.configure();
+        ExternalGlobalConfig globalConfig = new ExternalGlobalConfig(jenkins.getConfigPage());
+        globalConfig.addDiskPool(DISK_POOL_ID, DISK_ONE, DISK_TWO, MOUNT_FROM_MASTER_TO_DISK_ONE, MOUNT_FROM_MASTER_TO_DISK_TWO);
+        jenkins.save();
+    }
+
+    private void setUpNode(String label, String fakeMountingPoint) throws ExecutionException, InterruptedException {
+        SlaveController controller = new LocalSlaveController();
+        Slave linuxSlave = controller.install(jenkins).get();
+        linuxSlave.configure();
+        linuxSlave.setLabels(label);
+
+        ExternalNodeConfig nodeConfig = new ExternalNodeConfig(linuxSlave);
+        nodeConfig.setConfig(DISK_POOL_ID, DISK_ONE, DISK_TWO, fakeMountingPoint);
+        linuxSlave.save();
+    }
+
+    private WorkflowJob createWorkflowJob(String script) {
+        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
+        job.script.set(script);
+        job.save();
+
+        return job;
+    }
+
+    private void verifyExternalWorkspacesAction(String jobName, Build build) {
+        build.visit("exwsAllocate");
+        String exwsAllocateText = driver.findElement(By.id("main-panel")).getText();
+        assertThat(exwsAllocateText, containsString(String.format("Disk Pool ID: %s", DISK_POOL_ID)));
+        assertThat(exwsAllocateText, containsString(String.format("Disk ID: %s", DISK_ONE)));
+        assertThat(exwsAllocateText, containsString(String.format("Workspace path on %s: %s/%s", DISK_ONE, jobName, build.getNumber())));
+        assertThat(exwsAllocateText, containsString(String.format("Complete workspace path on %s (from Jenkins master): %s/%s/%s",
+                DISK_ONE, MOUNT_FROM_MASTER_TO_DISK_ONE, jobName, build.getNumber())));
+    }
+}
